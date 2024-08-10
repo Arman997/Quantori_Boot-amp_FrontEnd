c3e615ffae77ff4df43ca7bb04d8606c614671ab
TUMO Labs <tumolabs@TUMOs-iMac.local>
2024-08-11 01:07:37 +0400

    ubdate

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import 'bo.css'
+import './main.css'
 
 function Main() {
   return (
     <>
         <div className="main">
-                Hellloooooo World!!!!!
+          <div className='test1'>
+            <div className='test1div'>HELLO</div>
+            <div className='test1div'>HELLO</div>
+            <div className='test1div'>HELLO</div>
+          </div>
+          <div className='test2'>
+            <div className='test2div'>WORLD</div>
+            <div className='test2div'>WORLD</div>
+            <div className='test2div'>WORLD</div>
+          </div>
+          <div className='test3'>
+            <div>HOW ARE YOU</div>
+            <div>HOW ARE YOU</div>
+            <div>HOW ARE YOU</div>
+          </div>
+              
         </div>
     </>
   )
